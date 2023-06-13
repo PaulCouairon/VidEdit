9c6f6f4671ca8110d0fd7bd2f23b569f006023ee
Paul Couairon <paul.couairon@gmail.com>
2023-06-13 13:40:14 +0200

    videos update

--- assets/teaser-data.js
+++ assets/teaser-data.js
@@ -3,13 +3,13 @@ file_paths = {
     "bus-rusty": "./assets/data/gif/bus_rusty.gif",
     "bus-watercolor": "./assets/data/gif/bus_watercolor.gif",
 
-    "car-turn_retrowave": "./assets/data/gif/car-turn_retrowave.gif",
-    "car-turn_autumn": "./assets/data/gif/car-turn_autumn.gif",
-    "car-turn_rusty": "./assets/data/gif/car-turn_rusty.gif",
+    "car-turn-flowers": "./assets/data/gif/car-turn_flowers.gif",
+    "car-turn-autumn": "./assets/data/gif/car-turn_autumn.gif",
+    "car-turn-rusty": "./assets/data/gif/car-turn_rusty.gif",
 
     "motorbike-roses": "./assets/data/gif/motorbike_roses.gif",
     "motorbike-lake": "./assets/data/gif/motorbike_lake.gif",
-    "motorbike-snowy": "./assets/data/gif/motorbike_snowy.gif",
+    "motorbike-golden": "./assets/data/gif/motorbike_golden.gif",
 
     "kite-surf-santa": "./assets/data/gif/kite-surf_santa.gif",
     "kite-surf-vangogh": "./assets/data/gif/kite-surf_vangogh.gif",
